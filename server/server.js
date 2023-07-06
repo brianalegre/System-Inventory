0f377a90742cd05cedacdd79180c43831c61b7ba
Brian Alegre <brialegre@yahoo.com>
2023-07-05 21:56:46 -0700

    Created connection to sql database, installed mysql2, sequelize and dotenv

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@
 const express = require('express')
 const app = express()
 
+// SQL connection
+const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const PORT = process.env.PORT || 3001;
+
 // Create route
 app.get("/api", (req, res) => {
     res.json({ "users": ["userOne", "userTwo", "userThree"] })
@@ -9,3 +14,7 @@ app.get("/api", (req, res) => {
 
 // Tell the backend which port the backend started on
 app.listen(5000, () => { console.log("Server started on port 5000") })
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
