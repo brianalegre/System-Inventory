64011fa4b3252a2cda1c7084249dccd221d352d4
Brian Alegre <brialegre@yahoo.com>
2023-07-06 21:43:52 -0700

    Created Systems Model

---
+++ server/models/Systems.js
@@ -0,0 +1,34 @@
+// Import Model and Sequelize
+const { Model, DataTypes, INTEGER, BOOLEAN } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Create Systems Model
+class Systems extends Model { }
+
+Systems.init(
+    {
+        system_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        system_name: {
+            type: DataTypes.STRING(16),
+            allowNull: false,
+            unique: true,
+        },
+        ip_address: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            unique: true,
+            validat: {
+                isAlphaNumeric: true,
+            },
+        },
+        state: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+        },
+    }
+)
