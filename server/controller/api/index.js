a331f90de945bed3079289c9f11d0115da99c68d
Brian Alegre <brialegre@yahoo.com>
2023-07-07 21:15:31 -0700

    Created API call for Systems

---
+++ server/controller/api/index.js
@@ -0,0 +1,43 @@
+// Import the router, and the models
+const router = require('express').Router();
+const { Systems } = require('../../models')
+
+
+// Get All Systems
+// Endpoint /api/systems
+router.get('/', async (req, res) => {
+    try {
+        // Query All Systems
+        const getAllSystems = await Systems.findAll({
+        })
+
+        // Return data
+        res.status(200).json(getAllSystems)
+    } catch (err) {
+        res.status(500).json('Something went wrong', err);
+    }
+})
+
+// Get Single System
+// Endpoint /api/systems/:id
+router.get('/:system_id', async (req, res) => {
+    try {
+        // Query by system_id
+        const getSingleSystem = await Systems.findByPk(req.params.system_id, {
+        })
+
+        // If no system found
+        if (!getSingleSystem) {
+            res.status(404).json({ message: 'No System Found with that ID' })
+        }
+
+        // Return data
+        res.status(200).json(getSingleSystem)
+
+    } catch (err) {
+        res.status(500).json('getSingleSystem - something went wrong')
+    }
+})
+
+// Export
+module.exports = router;
