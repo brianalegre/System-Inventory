29fda5c08be97fc1feeb44be9f431129d9966052
Brian Alegre <brialegre@yahoo.com>
2023-07-13 21:36:35 -0700

    Added Tailwind

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,9 +6,9 @@ import React from 'react';
 function Navbar() {
     return (
         <div className='flex justify-between py-5 bg-slate-800 '>
-            <img src='' alt='logo' className='mx-10 h-6' />
+            <img src='' alt='logo1' className='mx-10 h-6' />
             <p className='text-white font-oleo-script text-2xl mx-10'>Inventory</p>
-            <img src='./images/wdLogo.png' alt='wdLogo' className='mx-10 h-10' />
+            <img src='' alt='logo2' className='mx-10 h-10' />
         </div>
     )
 
---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Import Google Font
+      fontFamily: {
+        'oleo-script': ['Oleo Script', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+
+}
+
