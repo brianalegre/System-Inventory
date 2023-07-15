f1f1a64f51e431a429b41c4b6850b165c6851796
Brian Alegre <brialegre@yahoo.com>
2023-07-15 16:13:07 -0700

    Testing endpoint on react

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 // Import
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
-
 
 // Import Components
 import Search from "./Components/Search";
@@ -10,6 +9,32 @@ import Navbar from "./Components/Navbars";
 
 // Main Application
 function App() {
+
+  // useState to store data from API
+  const [deviceInventory, setDeviceInventory] = useState([]);
+
+  // Function to fetch data from API
+  // const fetchInventory = async () => {
+  //   const response = await fetch("/api/systems");
+  //   const data = await response.json();
+  //   setDeviceInventory(data);
+  //   console.log('hi', data)
+  // };
+  const fetchInventory = () => {
+    fetch('/api/systems')
+      .then((response => response.json())
+        .then((data) => setDeviceInventory(data))
+        .catch((err) => console.log(err))
+      )
+  }
+
+
+  // useEffect to fetch data from API
+  // useEffect(() => {
+  //   fetchInventory();
+  // }, []);
+
+
   return (
     <div>
       <Navbar />
