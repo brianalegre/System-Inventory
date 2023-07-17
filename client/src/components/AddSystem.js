7eda696bb21f3b22ccc39deb77af04d87193393e
Brian Alegre <brialegre@yahoo.com>
2023-07-16 21:01:07 -0700

    Added notes to AddSystem Component

--- client/src/components/AddSystem.js
+++ client/src/components/AddSystem.js
@@ -4,6 +4,12 @@ import React from 'react';
 
 // Main Component
 function AddSystem() {
+
+    // Create button 'Add System'
+    // Opens Module
+    // Text Fields to fill out: system_name, ip_address
+    // Button to 'Create' and 'Cancel'
+
     return (
         <div>
 
