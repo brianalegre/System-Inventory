cc27040228b346fb7d928c41122dadbad891bf34
Brian Alegre <brialegre@yahoo.com>
2023-07-17 21:58:30 -0700

    installed react-modal

--- client/src/components/Navbars.js
+++ client/src/components/Navbars.js
@@ -1,6 +1,8 @@
 // Import React
 import React from 'react';
 
+// Import AddSystem
+import AddSystem from './AddSystem';
 
 // Navbar Component
 function Navbar() {
@@ -8,6 +10,7 @@ function Navbar() {
         <div className='flex justify-between py-5 bg-slate-800 '>
             {/* <img src='' alt='logo1' className='mx-10 h-6' /> */}
             <p className='text-white font-oleo-script text-2xl mx-10'>Inventory</p>
+            <AddSystem />
             {/* <img src='' alt='logo2' className='mx-10 h-10' /> */}
         </div>
     )
