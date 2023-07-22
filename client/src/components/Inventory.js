f3400bca5d1670eaf5de0ffa8248bc71bc1ae612
Brian Alegre <brialegre@yahoo.com>
2023-07-21 22:25:57 -0700

    Fixed key error for mapping in the inventory component

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -34,14 +34,13 @@ export default function Inventory({ deviceInventory }) {
 
     const inventoryData = deviceInventory.map((item) => (
 
-        <tbody className="bg-white divide-y divide-gray-200 text-center">
-            <tr key='system_id'>
+        <tbody key={item.system_id} className="bg-white divide-y divide-gray-200 text-center">
+            <tr>
                 <td className="px-6 py-4 whitespace-nowrap" data-id={item.system_id}>
                     <input type="checkbox"
                         value={item.system_id}
                         className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                         onChange={handleChange}
-
                     />
                 </td>
                 <td className="px-6 py-4 whitespace-nowrap">
