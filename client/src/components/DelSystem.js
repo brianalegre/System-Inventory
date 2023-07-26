c071e64a57f087789458fd24b3065efb0963559f
Brian Alegre <brialegre@yahoo.com>
2023-07-25 21:34:39 -0700

    Added check to see if checkbox is checked before deleting or editting a system

--- client/src/components/DelSystem.js
+++ client/src/components/DelSystem.js
@@ -54,27 +54,49 @@ export default function DelSystem({ checkboxState }) {
     return (
         <div>
             <button onClick={openModal} className="bg-red-400 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-36 m-1">Delete System</button>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-            >
-                <h2 className="flex justify-center font-bold underline m-1 p-1">Confirmation</h2>
-                <form className='px-8 pt-6 pb-8 mb-4'>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            Are you sure you want to delete this system?
-                        </label>
+            {checkboxState.length === 0 ? (
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                >
+                    <h2 className="flex justify-center font-bold underline m-1 p-1">Delete System</h2>
+                    <form className='px-8 pt-6 pb-8 mb-4'>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">
+                                You Must Select A System First!
+                            </label>
+                        </div>
+                    </form>
+                    <div className='flex justify-center'>
+                        <button onClick={closeModal}
+                            className="py-2.5 w-36 px-5 mr-2 mb-5 text-lg font-semibold focus:outline-none rounded-full text-center bg-green-300 hover:bg-green-700 text-black hover:text-gray-100 transition duration-300">
+                            OK</button>
                     </div>
-                </form>
-                <div className='flex justify-center'>
-                    <button onClick={submitModal}
-                        className="py-2.5 w-36 px-5 mr-2 mb-5 text-lg font-semibold focus:outline-none rounded-full text-center bg-green-300 hover:bg-green-700 text-black hover:text-gray-100 transition duration-300">
-                        Confirm</button>
-                    <button onClick={closeModal}
-                        className="py-2.5 w-36 px-5 mr-2 mb-5 text-lg font-semibold focus:outline-none rounded-full text-center bg-red-300 hover:bg-red-700 text-black hover:text-gray-100 transition duration-300">Cancel</button>
-                </div>
-            </Modal>
+                </Modal>
+            ) : (
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                >
+                    <h2 className="flex justify-center font-bold underline m-1 p-1">Confirmation</h2>
+                    <form className='px-8 pt-6 pb-8 mb-4'>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">
+                                Are you sure you want to delete this system?
+                            </label>
+                        </div>
+                    </form>
+                    <div className='flex justify-center'>
+                        <button onClick={submitModal}
+                            className="py-2.5 w-36 px-5 mr-2 mb-5 text-lg font-semibold focus:outline-none rounded-full text-center bg-green-300 hover:bg-green-700 text-black hover:text-gray-100 transition duration-300">
+                            Confirm</button>
+                        <button onClick={closeModal}
+                            className="py-2.5 w-36 px-5 mr-2 mb-5 text-lg font-semibold focus:outline-none rounded-full text-center bg-red-300 hover:bg-red-700 text-black hover:text-gray-100 transition duration-300">Cancel</button>
+                    </div>
+                </Modal>
+            )}
         </div>
     )
 }
--- client/src/components/EditSystem.js
+++ client/src/components/EditSystem.js
@@ -63,12 +63,10 @@ export default function EditSystem({ checkboxState }) {
         setFormState({ ...formState, [event.target.id]: event.target.value });
     }
 
-
-    // if checkboxState is empty, then return
-    if (checkboxState.length === 0) {
-        return (
-            <div>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-36 m-1">Edit System</button>
+    return (
+        <div>
+            <button onClick={openModal} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-36 m-1">Edit System</button>
+            {checkboxState.length === 0 ? (
                 <Modal
                     isOpen={modalIsOpen}
                     onRequestClose={closeModal}
@@ -88,53 +86,48 @@ export default function EditSystem({ checkboxState }) {
                             OK</button>
                     </div>
                 </Modal>
-            </div>
-        )
-    }
-
-
-    return (
-        <div>
-            <button onClick={openModal} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-36 m-1">Edit System</button>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={closeModal}
-                style={customStyles}
-            >
-                <h2 className="flex justify-center font-bold underline m-1 p-1">Edit System</h2>
-                <form className='px-8 pt-6 pb-8 mb-4'>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            System Name
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="system_name"
-                            type="text"
-                            placeholder="System Name"
-                            onChange={handleChange}
-                            value={formState.system_name} />
+            ) : (
+                <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                >
+                    <h2 className="flex justify-center font-bold underline m-1 p-1">Edit System</h2>
+                    <form className='px-8 pt-6 pb-8 mb-4'>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">
+                                System Name
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="system_name"
+                                type="text"
+                                placeholder="System Name"
+                                onChange={handleChange}
+                                value={formState.system_name} />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2">
+                                IP Address
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                id="ip_address"
+                                type="text"
+                                placeholder="IP Address"
+                                onChange={handleChange}
+                                value={formState.ip_address} />
+                        </div>
+                    </form>
+                    <div className='flex justify-center'>
+                        <button onClick={submitModal}
+                            className="py-2.5 w-36 px-5 mr-2 mb-5 text-lg font-semibold focus:outline-none rounded-full text-center bg-green-300 hover:bg-green-700 text-black hover:text-gray-100 transition duration-300">
+                            Confirm</button>
+                        <button onClick={closeModal}
+                            className="py-2.5 w-36 px-5 mr-2 mb-5 text-lg font-semibold focus:outline-none rounded-full text-center bg-red-300 hover:bg-red-700 text-black hover:text-gray-100 transition duration-300">Cancel</button>
                     </div>
-                    <div className="mb-4">
-                        <label className="block text-gray-700 text-sm font-bold mb-2">
-                            IP Address
-                        </label>
-                        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="ip_address"
-                            type="text"
-                            placeholder="IP Address"
-                            onChange={handleChange}
-                            value={formState.ip_address} />
-                    </div>
-                </form>
-                <div className='flex justify-center'>
-                    <button onClick={submitModal}
-                        className="py-2.5 w-36 px-5 mr-2 mb-5 text-lg font-semibold focus:outline-none rounded-full text-center bg-green-300 hover:bg-green-700 text-black hover:text-gray-100 transition duration-300">
-                        Confirm</button>
-                    <button onClick={closeModal}
-                        className="py-2.5 w-36 px-5 mr-2 mb-5 text-lg font-semibold focus:outline-none rounded-full text-center bg-red-300 hover:bg-red-700 text-black hover:text-gray-100 transition duration-300">Cancel</button>
-                </div>
-            </Modal>
+                </Modal>
+            )}
         </div>
+
     );
 }
 
