db66a5bf6a5cf1606558c12fde1eb4c1dcb7c249
Brian Alegre <brialegre@yahoo.com>
2023-07-30 23:14:34 -0700

    Updated search page

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,7 +1,68 @@
 import React from 'react'
 
-export default function Search() {
-  return (
-    <div>Search</div>
-  )
-}
+export default function Search(searchedTerm) {
+
+    const inventoryData = deviceInventory.map((item) => (
+        <tbody key={item.system_id} className="bg-white divide-y divide-gray-200 text-center">
+            <tr>
+                <td className="px-6 py-4 whitespace-nowrap" data-id={item.system_id}>
+                    <input type="checkbox"
+                        value={item.system_id}
+                        className="cursor-pointer w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        onChange={handleChange}
+                    />
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                    {item.system_id}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                    {item.system_name}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                    {item.ip_address}
+                </td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                    {item.state === true ? (
+                        "On"
+                    ) : (
+                        "Off"
+                    )}
+                </td>
+            </tr>
+        </tbody >
+    ))
+
+
+    return (
+        <div>
+            <div className='flex m-2 justify-center'>
+                <AddSystem />
+                <EditSystem checkboxState={checkboxState} />
+                <DelSystem checkboxState={checkboxState} />
+            </div>
+            <div>
+                <table className="min-w-full divide-y divide-gray-200">
+                    <thead className="bg-gray-50 text-center">
+                        <tr>
+                            <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Select
+                            </th>
+                            <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                System ID
+                            </th>
+                            <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                System Name
+                            </th>
+                            <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                IP Address
+                            </th>
+                            <th scope="col" className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                State
+                            </th>
+                        </tr>
+                    </thead>
+                    {inventoryData}
+                </table>
+            </div>
+        </div >
+
